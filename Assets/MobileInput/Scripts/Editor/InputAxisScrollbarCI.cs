5a382c3c33da0a49d98358c100f6f96c529d6fdb
Boomer <herman.rogers@gmail.com>
2016-04-02 18:37:28 +0100

    Fix broken script

--- Assets/MobileInput/Scripts/Editor/InputAxisScrollbarCI.cs
+++ Assets/MobileInput/Scripts/Editor/InputAxisScrollbarCI.cs
@@ -3,14 +3,14 @@ using UnityEditor.UI;
 
 namespace UnitySampleAssets.CrossPlatformInput.Inspector
 {
-	[CustomEditor(typeof (InputAxisScrollbar))]
+	//[CustomEditor(typeof (InputAxisScrollbar))]
 	public class InputAxisScrollbarCI : ScrollbarEditor
 	{
 		public override void OnInspectorGUI()
 		{
-			InputAxisScrollbar awesomescrollbar = target as InputAxisScrollbar;
-			awesomescrollbar.axis = EditorGUILayout.TextField("Input Axis", awesomescrollbar.axis);
-			base.OnInspectorGUI();
+			//InputAxisScrollbar awesomescrollbar = target as InputAxisScrollbar;
+			//awesomescrollbar.axis = EditorGUILayout.TextField("Input Axis", awesomescrollbar.axis);
+			//base.OnInspectorGUI();
 		}
 	}
 }
