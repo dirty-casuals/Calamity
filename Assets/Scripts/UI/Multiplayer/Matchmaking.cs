dad1a2612ba30a5c48af1749d52ad8e7ce2f171f
Boomer <herman.rogers@gmail.com>
2016-06-13 23:41:32 +0100

    Add multiplayer lobby initial layout

---
+++ Assets/Scripts/UI/Multiplayer/Matchmaking.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Network;
+
+public class Matchmaking : MonoBehaviour {
+
+    [HideInInspector]
+    public LobbyManager lobbyManager;
+
+    private void Start( ) {
+        lobbyManager = GetComponent<LobbyManager>( );
+    }
+
+    public void CreateGameAndHost( ) {
+        lobbyManager.StartHost( );
+    }
+}
