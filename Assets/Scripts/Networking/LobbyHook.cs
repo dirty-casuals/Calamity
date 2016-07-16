b45e78f0422e04aa8354fa7548db3bea8647017c
Chuck Dixon <chvckd@gmail.com>
2016-07-16 19:07:47 +0100

    Rework the lobbymanager stuff so that it's also used for single player and a new placeholder player prefab

---
+++ Assets/Scripts/Networking/LobbyHook.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public abstract class LobbyHook : MonoBehaviour {
+    public virtual void OnLobbyServerSceneLoadedForPlayer( NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer ) { }
+}
