822c250abbb952037a85b5efb170f57f671b52cd
Chuck Dixon <chvckd@gmail.com>
2016-09-22 18:31:03 +0100

    Add a simple fpslock object

---
+++ Assets/Scripts/FpsLock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FpsLock : MonoBehaviour {
+
+    [SerializeField]
+    private int targetFPS = 45;
+    
+	private void Start () {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = targetFPS;
+    }
+}
