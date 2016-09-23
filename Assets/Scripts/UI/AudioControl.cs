7a79a3d15980861b64e417e2eac5652cb5cc7feb
Chuck Dixon <chvckd@gmail.com>
2016-09-23 22:15:13 +0100

    Alter the audio control so it can work with in game pause menu

--- Assets/Scripts/UI/AudioControl.cs
+++ Assets/Scripts/UI/AudioControl.cs
@@ -7,11 +7,18 @@ public class AudioControl : MonoBehaviour {
     [SerializeField]
     private GameObject offObject;
 
+    private static AudioControl instance = null;
     private static bool audioEnabled = true;
 
-    private void Awake( ) {
-        audioEnabled = true;
+    public static AudioControl Instance
+    {
+        get {
+            return instance;
+        }
+    }
 
+    private void Awake( ) {
+        offObject.SetActive( !audioEnabled );
     }
 
     private void OnMouseDown( ) {
