a1ecd4ffdd886af0380e22c539c9e9c20d2ca8b7
chvck <chvckd@gmail.com>
2018-11-03 11:06:06 +0000

    Initial project creation

---
+++ Source/calamity/calamityGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "calamityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALAMITY_API AcalamityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
